2fbd9d7925ac2ee21039629a536a18b68d260507
mathematicw <wilyam@protonmail.com>
2024-10-03 15:48:40 +0700

    Initial commit

---
+++ .pythonlibs/lib/python3.11/site-packages/htmldate/__init__.py
@@ -0,0 +1,19 @@
+"""
+Htmldate extracts original and updated publication dates from URLs and web pages.
+"""
+
+# meta
+__title__ = "htmldate"
+__author__ = "Adrien Barbaresi"
+__license__ = "Apache-2.0"
+__copyright__ = "Copyright 2017-2024, Adrien Barbaresi"
+__version__ = "1.9.0"
+
+
+import logging
+
+from datetime import datetime
+
+from .core import find_date
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
